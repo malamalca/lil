ff8725f834a32a4adb05636676c14738d0366f35
Miha Nahtigal <miha.nahtigal@gmail.com>
2016-11-06 18:13:03 +0100

    Moved trait

---
+++ src/Model/Table/IsOwnedByTrait.php
@@ -0,0 +1,21 @@
+<?php
+namespace Lil\Model;
+
+trait IsOwnedByTrait
+{
+    /**
+     * Returns default counter
+     *
+     * @param uuid $entityId Entity id.
+     * @param uuid $ownerId Owner Id.
+     * @param string $ownerField Owner field.
+     * @return mixed
+     */
+    public function isOwnedBy($entityId, $ownerId, $ownerField = 'owner_id')
+    {
+        $conditions = ['id' => $entityId, $ownerField => $ownerId];
+        $ret = $this->exists($conditions);
+
+        return $ret;
+    }
+}
